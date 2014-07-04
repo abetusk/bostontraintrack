9f736150f39ef46a1ec396c278d49624de27425a
abetusk <abetusk@gmail.com>
2014-07-04 01:22:03 -0400

    Added AGPLv3 header.

--- www/js/bostontraintrack.js
+++ www/js/bostontraintrack.js
@@ -1,3 +1,24 @@
+/*
+
+    Copyright (C) 2013 Abram Connelly
+
+    This file is part of bostontraintrack.
+
+    bostontraintrack is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    bostontraintrack is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with bostontraintrack.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 var g_stop_layer ;
 
 var g_verbose=1;

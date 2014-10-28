1cd4294ba97fd8ba94f378b568423da71e8865c9
abetusk <abetusk@gmail.com>
2014-10-28 08:05:09 -0400

    srv_v2.js: using GTRTFS feed to get all position data.  Commuter seems   intermittant and buses do not have heading information. srv_bus_v2.js: added some fields and changed feed names to reflect   updated feed names.

---
+++ srv/srv_bus_v2.js
@@ -0,0 +1,428 @@
+/*
+
+    Copyright (C) 2013 Abram Connelly
+
+    This file is part of bostontraintrack.
+
+    bostontraintrack is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    bostontraintrack is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with bostontraintrack.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+var http = require("http");
+var sockio = require('socket.io')();
+var xmlparse = require("xml2js").parseString;
+
+var g_verbose = 0;
+
+var global_connect = { };
+var global_status = { };
+
+var g_bus_data = {};
+
+var global_data = { n: 0,
+                    interval : 10000,
+                    port : 80,
+                    url : "webservices.nextbus.com",
+                    path : "/service/publicXMLFeed?command=vehicleLocations&a=mbta&t=0"
+                  };
+
+
+function flushSingleClientData( cli_id )
+{
+
+  console.log(">>>>> flushing", cli_id );
+
+  for (var bus_id in g_bus_data)
+  {
+
+    //console.log("  ", bus_id, ">>");
+
+    var bus_data = g_bus_data[bus_id];
+    var s = JSON.stringify( bus_data );
+
+    try 
+    {
+      global_connect[ cli_id ].emit( "update:bus", bus_data );
+    } catch (ee) {
+      console.log("ERROR: flushing to client", cli_id, "(bus_id ", bus_id , ") failed");
+    }
+
+  }
+
+}
+
+
+function countconn() {
+  var conn_count=0;
+  for (var cli_id in global_connect) { conn_count++; }
+  return conn_count;
+}
+
+function countenable() {
+  var enable_count=0;
+  for (var cli_id in global_connect) {
+    try {
+      if (global_status[ cli_id ].enable) { enable_count++; }
+    } catch (ee) { return -1; }
+  }
+  return enable_count;
+}
+
+function pushData( vehicle_data ) 
+{
+
+  var conn_count = 0;
+  var enable_count=0;
+
+  var dt = Date.now();
+  var dts = dt.toString();
+
+
+  for (var cli_id in global_connect) 
+  {
+    conn_count++;
+
+    try 
+    {
+      if (global_status[ cli_id ].enable) 
+      {
+        enable_count++;
+
+        console.log("  pushing " + cli_id + ">>>" + vehicle_data.id );
+
+        global_connect[ cli_id ].emit( "update:bus", vehicle_data );
+      }
+    } catch (ee) {
+      console.log("ERROR: pushing to client", cli_id, "(vehicle_id", vehicle_id, ") failed");
+    }
+
+  }
+
+  //console.log( dts +  " connected clients:", conn_count + ", (" +  global_data.n + "), (enabled: " +  enable_count +  "), vehicle_id:", vehicle_data.id );
+
+
+}
+
+
+function updateBus( body ) 
+{
+
+  console.log("updateBus:");
+  //console.log( body );
+
+  if (!("body" in body)) { return; }
+  if (!("vehicle" in body["body"])) { return; }
+
+  var vehicle = body["body"]["vehicle"];
+
+  for ( var vehicle_id in g_bus_data)
+  {
+    g_bus_data[vehicle_id].status = "delete";
+  }
+
+
+  var dt  = Date.now();
+
+
+  for (var ind in vehicle){
+    //console.log( ind, vehicle[ind] );
+
+    if (!("$" in vehicle[ind])) { continue; }
+    var dat = vehicle[ind]["$"];
+
+    if (!("routeTag" in dat)) { continue; }
+    if (!("lat" in dat)) { continue; }
+    if (!("lon" in dat)) { continue; }
+    if (!("id" in dat)) { continue; }
+
+    var bus_id = dat.id;
+    var route = dat.routeTag;
+    var latstr = dat.lat;
+    var lonstr = dat.lon;
+    var heading = dat["heading"];
+    var secstr = dat["secsSinceReport"];
+
+    if (secstr == "") { secstr = "0"; }
+
+    var sec = parseInt(secstr);
+    var ts = dt - sec;
+
+    if ( (latstr == "") || (lonstr == "") ) 
+    { 
+      console.log("  " + bus_id + ">>> DEL");
+      g_bus_data[bus_id] = { id : bus_id, route : route, status : "delete" }; 
+      continue;
+    }
+
+    var lat = parseFloat(latstr);
+    var lon = parseFloat(lonstr);
+
+
+    if (bus_id in g_bus_data)
+    {
+      if ( (Math.abs(lat - g_bus_data[bus_id].lat) > 0.001) ||
+           (Math.abs(lon - g_bus_data[bus_id].lon) > 0.001) )
+      {
+
+        var dlat = Math.abs(lat - g_bus_data[bus_id].lat);
+        var dlon = Math.abs(lon - g_bus_data[bus_id].lon);
+
+        console.log("  update>>", bus_id, lat, lon, dlat, dlon, heading); 
+
+        g_bus_data[bus_id].timestamp = ts;
+        g_bus_data[bus_id].lat = lat;
+        g_bus_data[bus_id].lon = lon;
+        g_bus_data[bus_id].status = "update";
+        g_bus_data[bus_id].heading = heading;
+        g_bus_data[bus_id].id = bus_id;
+        g_bus_data[bus_id].route = route;
+        g_bus_data[bus_id].route_id = route;
+        g_bus_data[bus_id].route_name = route;
+        g_bus_data[bus_id].route_group = route;
+      } else  {
+        if (g_bus_data[bus_id].status == "delete") {
+          g_bus_data[bus_id].status = "idle";
+        }
+      }
+
+    } else {
+
+      g_bus_data[bus_id] = { 
+        id : bus_id, 
+        timestamp : ts,
+        route: route, 
+        route_id: route, 
+        route_group: route, 
+        route_name: route, 
+        lat : lat, 
+        lon : lon, 
+        status : "new", 
+        heading : heading
+      };
+
+      console.log("  new>>", bus_id, lat, lon, heading);
+    }
+
+  }
+
+  var remove_list = [];
+
+  for ( var bus_id in g_bus_data )
+  {
+
+    if ( g_bus_data[bus_id].status != "idle" ) {
+      pushData( g_bus_data[bus_id] );
+    }
+    if (g_bus_data[bus_id].status == "delete") { remove_list.push(bus_id); }
+  }
+
+  for (var ind=0; ind<remove_list.length; ind++)
+  {
+    console.log("  delete>>", remove_list[ind] );
+    delete g_bus_data[ remove_list[ind] ];
+  }
+
+
+
+
+  return;
+
+  var json_data;
+  try  { json_data = JSON.parse(body); } 
+  catch (ee) { console.log("json parse error:", ee ); return; }
+
+  var data = g_bus_data[bus_id];
+
+  for ( var vehicle_id in data )
+  {
+    data[vehicle_id].status = "delete";
+  }
+
+  // Save it in case we want it
+  //
+  global_data[bus_id] = json_data;
+
+  for (var ind in json_data["Messages"])
+  {
+    var entry = json_data.Messages[ind];
+    var vid = entry["Vehicle"];
+
+    //if (vid != "" ){ console.log("GOT VID:", vid); }
+
+    latstr = entry["Latitude"];
+    lonstr = entry["Longitude"];
+
+    if (vid == "") 
+    {
+      //console.log("empty vid, skipping");
+      continue;
+    }
+
+    if ( (latstr == "") || (lonstr == "") ) 
+    { 
+
+      console.log("  " + bus_id + ">>> " + vid + " DEL");
+
+      data[vid] = { id : vid, route : linename, status : "delete" }; 
+      continue;
+    }
+
+    var lat = parseFloat(entry["Latitude"]);
+    var lon = parseFloat(entry["Longitude"]);
+
+    console.log("   " + linename + ">>>>" + vid + ", (" + lat + ", " + lon + ") [" + latstr + " " + lonstr + "] " + entry["Heading"]  );
+
+    //console.log("vid in data?", (vid in data) );
+
+    if (vid in data)
+    {
+      if ( (Math.abs(lat - data[vid].lat) > 0.001) ||
+           (Math.abs(lon - data[vid].lon) > 0.001) )
+      {
+
+        var dlat = Math.abs(lat - data[vid].lat);
+        var dlon = Math.abs(lon - data[vid].lon);
+
+        console.log("  update>>", vid, lat, lon, dlat, dlon, entry["Heading"]);
+
+        data[vid].timestamp = entry["TimeStamp"];
+        data[vid].lat = lat;
+        data[vid].lon = lon;
+        data[vid].status = "update";
+        data[vid].heading = entry["Heading"];
+        data[vid].id = vid;
+        data[vid].route = linename;
+      } else  {
+
+        if (data[vid].status == "delete") {
+          data[vid].status = "idle";
+        }
+
+      }
+
+
+    } else {
+
+      data[vid] = { id : vid, timestamp : entry["TimeStamp"], route: linename, lat : lat, lon : lon, status : "new", heading : entry["Heading"] };
+
+      console.log("  new>>", vid, lat, lon, entry["Heading"] );
+    }
+
+  }
+
+  var remove_list = [];
+
+  for ( var vid in data )
+  {
+
+    if ( data[vid].status != "idle" ) {
+      pushData( data[vid] );
+    }
+    if (data[vid].status == "delete") { remove_list.push(vid); }
+  }
+
+  for (var ind=0; ind<remove_list.length; ind++)
+  {
+    delete data[ remove_list[ind] ];
+  }
+
+
+}
+
+function debugPrint() {
+  console.log("DEBUG");
+  for (var route in g_line_vehicle_data) {
+    var train_data = g_line_vehicle_data[route];
+    for (var vid in train_data) {
+      console.log( vid, train_data[vid] );
+    }
+  }
+}
+
+function fetchBus() {
+
+  var opt = { host: global_data.url, port:global_data.port, path: global_data.path };
+
+  console.log("opt:", opt);
+
+  try {
+    var req = http.request( opt, function(res) {
+      res.setEncoding('utf8');
+      var body = '';
+      res.on('data', function(chunk) { body += chunk; });
+      res.on('end', function() { 
+        //console.log(body);
+        try {
+          xmlparse( body, function(err, result) {
+            if (err) { console.log("got err:", err); return; }
+            updateBus(result);
+          });
+        } catch (ee) {
+          console.log("xml parse error:", ee );
+        }
+      });
+    });
+    req.on('error', function(err) { console.log("got http erro:", err); });
+    req.end();
+  } catch (err2) {
+    console.log("http request error? " + err2);
+  }
+
+  var conn_count   = countconn();
+  var enable_count = countenable();
+
+  var dt  = Date.now();
+  var dts = dt.toString();
+
+  console.log( dts +  " connected clients:", conn_count, ", (", global_data.n, "), (enabled: ", enable_count, ")" );
+
+}
+
+// Do an initial burst to setup all commuter lines
+//
+fetchBus();
+
+setInterval( fetchBus, global_data.interval );
+
+sockio.on('connection', function(socket) { 
+  console.log("connection!"); 
+
+  global_data.n++;
+  var local_name = global_data.n;
+  global_connect[ local_name ] = socket;
+  global_status [ local_name ] = { enable : false };
+
+  socket.on("enable:bus", function(msg) {
+    console.log("enable:", local_name);
+    global_status[ local_name ].enable = true;
+    flushSingleClientData( local_name );
+  });
+
+  socket.on("disable:bus", function(msg) {
+    console.log("disable:", local_name);
+    global_status[ local_name ].enable = false;
+  });
+
+  socket.on("disconnect", function() { 
+    console.log("disconnecting client ", local_name); 
+    delete global_connect[ local_name ];
+    delete global_status [ local_name ];
+
+  });
+
+});
+
+sockio.listen(8182);
+
---
+++ srv/srv_v2.js
@@ -0,0 +1,403 @@
+/*
+
+    Copyright (C) 2013 Abram Connelly
+
+    This file is part of bostontraintrack.
+
+    bostontraintrack is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    bostontraintrack is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with bostontraintrack.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// Updated version of subway server to transmit only deltas.
+// Modeled off of the commuter rail server.
+
+var http = require("http");
+var fs = require("fs");
+var sockio = require('socket.io')();
+var protobuf = require("protobufjs");
+var builder = protobuf.loadProtoFile("gtfs-realtime.proto");
+var feedmessage = builder.build("transit_realtime.FeedMessage");
+
+var g_route_info = JSON.parse( fs.readFileSync('route_info.json', 'utf8') );
+
+var g_verbose = 1;
+var g_debug = 0;
+
+var g_mbta_type = [ "", "subway", "commuter", "bus" ];
+
+var global_data = {};
+var global_connect = { };
+var global_status = { };
+
+// has information by vehicle id
+//
+var g_vehicle_data = { };
+
+var g_total_connections = 0;
+var g_interval = 10000;
+var g_http_mbta_opt = { host:"developer.mbta.com", port:80, path:"/lib/GTRTFS/Alerts/VehiclePositions.pb" };
+
+function flushSingleClientData( cli_id, feed_type )
+{
+  var update_feed = ((typeof feed_type === "undefined") ? "update" : ("update:" + feed_type));
+  var feed = ((typeof feed_type === "undefined") ? "" : feed_type );
+
+  var all_feed = false;
+  if (!feed_type) { all_feed = true; }
+
+  //if (g_verbose) { console.log(">>>>> flushing", cli_id ); }
+
+  for (var vid in g_vehicle_data)
+  {
+
+    if ((!all_feed) && 
+        (g_vehicle_data[vid].route_group != feed)) {
+      continue;
+    }
+
+    var s = JSON.stringify( g_vehicle_data[vid] );
+
+    //if (g_verbose) { console.log("      ", vid, update_feed, ">>>", s); }
+
+    try 
+    {
+      global_connect[ cli_id ].emit( update_feed, g_vehicle_data[vid] );
+    } catch (ee) {
+      if (g_verbose) {
+        console.log("ERROR: flushing to client", cli_id, "(vehicle_id", vid , ") failed");
+      }
+    }
+
+  }
+}
+
+function countconn() {
+  var conn_count=0;
+  for (var cli_id in global_connect) { conn_count++; }
+  return conn_count;
+}
+
+function countenable( feed_type ) {
+  var feed = ((typeof feed_type === "undefined") ? "" : (":" + feed_type));
+  var enable_count=0;
+  for (var cli_id in global_connect) {
+    try {
+      if (global_status[ cli_id + feed].enable) { enable_count++; }
+    } catch (ee) {
+      console.log(ee, cli_id, feed);
+      return -1; }
+  }
+  return enable_count;
+}
+
+// Push a single vehicle update to all clients.
+//
+function pushData( vehicle_data ) 
+{
+  var conn_count = 0;
+  var enable_count = [0,0,0,0];
+  var dts = Date.now().toString();
+
+  for (var cli_id in global_connect) 
+  {
+    conn_count++;
+
+    for (var f_ind=0; f_ind<g_mbta_type.length; f_ind++) {
+      var feed = g_mbta_type[f_ind];
+      if (feed.length > 0) {
+        if (vehicle_data.route_group != feed) { continue; }
+        feed = ":" + feed;
+      }
+
+      try 
+      {
+        if (global_status[ cli_id + feed ].enable) 
+        {
+          if (g_verbose) {
+            console.log(dts, "push>>>> cli", cli_id, "feed", feed, "vid", vehicle_data.id );
+          }
+
+          enable_count[f_ind]++;
+          global_connect[ cli_id ].emit( "update" + feed, vehicle_data );
+        }
+      } catch (ee) {
+        console.log("ERROR: pushing to client", cli_id, "(vehicle_id", vehicle_id, ") failed");
+      }
+    }
+  }
+}
+
+
+function debugPrint() {
+  console.log("DEBUG");
+  for (var route in g_vehicle_data) {
+    var train_data = g_vehicle_data[route];
+    for (var vid in train_data) {
+      console.log( vid, train_data[vid] );
+    }
+  }
+}
+
+function updateVehiclePositions( buf ) {
+
+  var fm = feedmessage.decode(buf);
+  if (!fm) {
+    if (g_verbose) {
+      console.log("ERROR: updateVehiclePositions could not decode buffer");
+    }
+    return;
+  }
+
+  var ent = fm.entity;
+  var n = ent.length;
+
+  // We sweep through and mark every entry as 'delete'.
+  // If we encounter it below, we will mark it as 'update'.
+  // New entries will be added and marked as 'new'.
+  //
+  for ( var vehicle_id in g_vehicle_data )
+  {
+    g_vehicle_data[vehicle_id].status = "delete";
+  }
+
+  // Walk our enttity list and update our vehicle data
+  // structure.
+  //
+  for ( var i=0; i<n; i++ )
+  {
+
+    if (g_debug) { console.log( "ent[", i, "]:", ent[i]); }
+
+    var veh = ent[i].vehicle;
+
+    var vid = veh.vehicle.id;
+    var lat = veh.position.latitude;
+    var lon = veh.position.longitude;
+    var heading = veh.position.bearing;
+    var route_id = veh.trip.route_id;
+    var trip_id = veh.trip.trip_id;
+    var timestr = veh.timestamp.low;
+
+    var route_name = "?";
+    var route_group = "?";
+    var route_info = "?";
+    if ( route_id in g_route_info ) {
+      route_name = g_route_info[route_id]["route_name"];
+      route_group = g_route_info[route_id]["route_group"];
+      route_info = g_route_info[route_id]["info"];
+    }
+
+    if (g_verbose) {
+      //console.log("veh>>", vid,  route_name, route_group, route_id, "(", lat, lon, heading, ")" );
+    }
+
+    if (vid in g_vehicle_data)
+    {
+      if ( (Math.abs(lat - g_vehicle_data[vid].lat) > 0.001) ||
+           (Math.abs(lon - g_vehicle_data[vid].lon) > 0.001) )
+      {
+
+        var dlat = Math.abs(lat - g_vehicle_data[vid].lat);
+        var dlon = Math.abs(lon - g_vehicle_data[vid].lon);
+
+        //if (g_debug) { console.log("  update>>", vid, lat, lon, dlat, dlon, heading ); }
+        //if (g_verbose) { console.log("  update>>", vid, lat, lon, dlat, dlon, heading ); }
+
+        g_vehicle_data[vid].timestamp = timestr;
+        g_vehicle_data[vid].lat = lat;
+        g_vehicle_data[vid].lon = lon;
+        g_vehicle_data[vid].status = "update";
+        g_vehicle_data[vid].heading = heading;
+        g_vehicle_data[vid].id = vid;
+        g_vehicle_data[vid].route_id = route_id ;
+        g_vehicle_data[vid].route = route_name;
+        g_vehicle_data[vid].trip_name = trip_id;
+        g_vehicle_data[vid].route_group = route_group;
+        g_vehicle_data[vid].route_info = route_info;
+      } else  {
+
+        if (g_vehicle_data[vid].status == "delete") {
+          g_vehicle_data[vid].status = "idle";
+        }
+
+      }
+
+    } else {
+
+      g_vehicle_data[vid] = {
+        id : vid,
+        timestamp : timestr,
+        route : route_name,
+        route_id : route_id,
+        route_group : route_group,
+        route_info : route_info,
+        trip_name : trip_id,
+        lat : lat,
+        lon : lon,
+        status : "new",
+        heading : heading };
+
+      //if (g_debug) { console.log("  new>>", vid, lat, lon, heading ); }
+      //if (g_verbose) { console.log("  new>>", vid, lat, lon, heading ); }
+
+    }
+
+  }
+
+  // We need removal to be atomic, so put all
+  // vehicles that are marked for delition into
+  // a list then delete afterwards.
+  //
+  var remove_list = [];
+  for ( var vid in g_vehicle_data )
+  {
+
+    // For anythign other than an 'idle' message (that is,
+    // a 'new' or 'delete'), push it to the client.
+    //
+    if ( g_vehicle_data[vid].status != "idle" ) {
+      pushData( g_vehicle_data[vid] );
+    }
+
+    if (g_vehicle_data[vid].status == "delete") { remove_list.push(vid); }
+  }
+
+  for (var ind=0; ind<remove_list.length; ind++)
+  {
+
+    if (g_debug) { console.log("  delete>>>", remove_list[ind] ); }
+
+    g_vehicle_data[ remove_list[ind] ] = null;
+    delete g_vehicle_data[ remove_list[ind] ];
+  }
+
+}
+
+// Makes the request to the MBTA real time server, collects
+// the chunks of the (binary data) response and calls 
+// 'updateVehiclePositions' when the data is ready.
+//
+function fetchVehiclePositions() {
+
+  try {
+    var req = http.request( g_http_mbta_opt, function(res) {
+      var body = new Buffer([]);
+      res.on('data', function(chunk) {
+        var c = new Buffer( chunk );
+        var b = Buffer.concat([ body, c ]);
+        body = b;
+      });
+      res.on('end', function() { updateVehiclePositions( body ); });
+    });
+    req.on('error', function(err) {
+      if (g_verbose) {
+        console.log("got http error:", err);
+      }
+    });
+    req.end();
+  } catch (err2) {
+
+    if (g_verbose) {
+      console.log("http request error? " + err2);
+    }
+  }
+
+  var c = countconn();
+  var e = countenable();
+  
+  var dt = Date.now();
+  var dts = dt.toString();
+
+  if (g_verbose) {
+    console.log( dts +  " connected clients: " + c + " (" + e + ") [" + g_total_connections + "]" );
+  }
+
+
+}
+
+// Do an initial burst to setup all commuter lines
+//
+fetchVehiclePositions();
+
+// And set up our polling to fetch new data every
+// g_inteval seconds.
+//
+setInterval( fetchVehiclePositions, g_interval );
+
+// We want clients to subscribe to individual feeds ('subway',
+// 'bus', 'commuter' or all) to limit bandwidth.  We keep
+// a 'global_status' hash map that tells us whether the clients
+// are enabled for that feed.  The key being the 'local_name'
+// (the connection number) with the type of feed concatenated
+// (with a colon separation).  We let the client subscribe
+// to everything and the keys are just the local_name.
+//
+// Clients subscribe via 'enable(:subway|:bus|:commuter)?' and
+// unsubscribe via 'disable(:subway|:bus|:commuter)?' socket.io
+// messages.
+//
+// On enable, the state is flushed.  there is a setInterval above
+// that polls every g_interval millisecnds the MBTA feed.
+//
+sockio.on('connection', function(socket) { 
+
+  if (g_verbose) { console.log("connection!"); }
+
+  g_total_connections++;
+  var local_name = g_total_connections;
+  global_connect[ local_name ] = socket;
+
+  for ( var i=0; i<g_mbta_type.length; i++) {
+    var tt = ( (g_mbta_type[i].length > 0) ? (":" + g_mbta_type[i]) : "" );
+    var pp = ( (g_mbta_type[i].length > 0) ? (g_mbta_type[i]) : undefined );
+
+    global_status[ local_name + tt ] = { enable : false };
+
+    socket.on("enable" + tt,
+      (function( feed_mod, feed_type ) {
+        return function(msg) {
+          if (g_verbose) { console.log("enable" + feed_mod, local_name, feed_mod, feed_type); }
+          global_status[ local_name + feed_mod ].enable = true;
+          flushSingleClientData( local_name, feed_type );
+          };
+      })( tt, pp )
+    );
+
+    socket.on("disable" + tt,
+      (function( feed_mod, feed_type) {
+        return function(msg) {
+          if (g_verbose) { console.log("disable" + feed_mod, local_name, feed_mod, feed_type ); }
+          global_status[ local_name + feed_mod ].enable = false;
+        };
+      })( tt, pp )
+    );
+
+  }
+
+  socket.on("disconnect", function() { 
+    if (g_verbose) { console.log("disconnecting client ", local_name); }
+
+    delete global_connect[ local_name ];
+
+    for ( var i=0; i<g_mbta_type.length; i++) {
+      var tt = ( (g_mbta_type[i].length > 0) ? (":" + g_mbta_type[i]) : "" );
+      delete global_status[ local_name + tt];
+    }
+
+  });
+
+});
+
+sockio.listen(8181);
+
